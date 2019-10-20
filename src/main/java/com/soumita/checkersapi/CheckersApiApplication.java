fb96fd11ce47d4c211f2acc7375dfb7d6c07c8a2
Soumita Das <sd655@njit.edu>
2019-10-19 21:17:54 -0400

    Initial commit

---
+++ src/main/java/com/soumita/checkersapi/CheckersApiApplication.java
@@ -0,0 +1,13 @@
+package com.soumita.checkersapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckersApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soumita/checkersapi/CheckersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soumita.checkersapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
